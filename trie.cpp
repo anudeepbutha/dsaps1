b87d40ba78080f48581c8d3ec4fb2cfd04277041
anudeepbutha <111654636+anudeepbutha@users.noreply.github.com>
2025-08-23 18:09:36 +0530

    trieflag1,2

--- trie.cpp
+++ trie.cpp
@@ -8,6 +8,11 @@ struct Node {
     bool flag = false;
 };
 
+struct queries {
+    int op;
+    string queryword;
+};
+
 Node* root;
 
 void insert (string word) {
@@ -56,32 +61,66 @@ void operation2 (string word) {
     vector<string> suggestion;
     printRecursive(word, current, suggestion);
 
+    cout << suggestion.size() << endl;
     for (string str: suggestion)
         cout << str << endl;
 }
 
+// void operation3 (string word) {
+//     int wordlength = word.length();
+//     vector<int> beforeRow(wordlength+1, 0);
+//     for (int i=0; i<=wordlength; i++)
+//         beforeRow[i] = i;
+
+//     vector<int> currentRow(wordlength+1, 0);
+//     for (int i=0; i<=wordlength; i++)
+//         currentRow[i] = beforeRow[i] + 1;
+    
+//     int insert_cost, delete_cost, replace_cost;
+//     for (int j=0; j<wordlength; j++) {
+//         insert_cost = currentRow[j-1] + 1;
+//         delete_cost = beforeRow[j] + 1;
+//         replace_cost = beforeRow[j-1] + (pattern[j-1] != c ? 1 : 0);
+//         currentRow = min(insert_cost, delete_cost, replace_cost );
+//     }
+// }
+
 int main() {
     int wordcount, querycount;
-    // cin >> wordcount >> querycount;
-    // cin.ignore();
-    // vector<string> words;
-    // for (int i=0; i<wordcount; i++) {
-    //     string tmp;
-    //     getline(cin, tmp);
-    //     words.push_back(tmp);
-    // }
+    cin >> wordcount >> querycount;
+    cin.ignore();
+
+    vector<string> words;
+    for (int i=0; i<wordcount; i++) {
+        string tmp;
+        getline(cin, tmp);
+        words.push_back(tmp);
+    }
+
+    vector<queries> query_vector;
+    for (int i=0; i<querycount; i++) {
+        int num;
+        string str;
+        queries query;
+        cin >> num >> str;
+        cin.ignore();
+        query.op = num;
+        query.queryword = str;
+        query_vector.push_back(query);
+    }
+
     root = new Node;
-    // for (string str: words)
-        insert("consider");
-        insert("filters");
-        insert("filers");
-        insert("entitled");
-        insert("tilers");
-        insert("litter");
-        insert("dames");
-        insert("filling");
-        insert("grasses");
-        insert("fitter");
-        operation2("fil");
+    for (string str: words)
+        insert(str);
+
+    for (queries q: query_vector)
+        if (q.op == 1)
+            cout << operation1(q.queryword) << endl;
+        else if (q.op == 2)
+            operation2(q.queryword);
+        // else if (q.op == 3)
+        //     operation3(q.queryword);
+        else
+            cout << "invalid input" << endl;
     return 0;
 }
